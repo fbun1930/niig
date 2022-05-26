6b7e8661e4d31b926d0c7013135e8ec37fbe88e3
Rsbrodov <rsbrodov@mail.ru>
2022-05-26 15:21:19 +0700

    CREATE header file and structure of headers

---
+++ resources/js/components/layouts/Default.vue
@@ -0,0 +1,204 @@
+<template>
+    <div>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1">
+            <link rel="preconnect" href="https://fonts.gstatic.com/">
+            <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+            <meta name="description" content="Официальный сайт ФБУН Новосибирский НИИ гигены Роспотребнадзора"/>
+            <link rel="canonical" href="index.html" />
+            <meta property="og:locale" content="ru_RU" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="ФБУН Новосибирский НИИ гигены Роспотребнадзора" />
+            <meta property="og:description" content="ФБУН Новосибирский НИИ гигены Роспотребнадзора" />
+            <meta property="og:url" content="index.html" />
+            <meta property="og:site_name" content="" />
+            <meta property="og:image" content="dops/uploads/2021/07/Logotip-250-pks-na-250-pks-.png" />
+            <meta property="og:image:width" content="250" />
+            <meta property="og:image:height" content="250" />
+            <meta property="og:image:type" content="image/png" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <script type="application/ld+json" class="yoast-schema-graph"></script>
+            <meta name="google-site-verification" content="PTepyoGRHgdBUATUvcp15i9FcGwbqaufKy7THfsomuY" />
+            <meta name="yandex-verification" content="f0a59aec0f2bec26" />
+
+
+            <link rel='dns-prefetch' href='http://www.google.com/' />
+            <link rel='dns-prefetch' href='http://fonts.googleapis.com/' />
+            <link rel='dns-prefetch' href='http://s.w.org/' />
+            <link rel="alternate" type="application/rss+xml" title="" />
+
+
+
+
+        <link rel='stylesheet' id='dnd-upload-cf7-css'  href='https://niig/dops/plugins/drag-and-drop-multiple-file-upload-contact-form-7/assets/css/dnd-upload-cf7.css' type='text/css' media='all' />
+        <link rel='stylesheet' id='contact-form-7-css'  href='https://niig/dops/plugins/contact-form-7/includes/css/styles.css' type='text/css' media='all' />
+        <link rel='stylesheet' id='google-fonts-css'  href='https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,500;0,600;1,300&amp;family=Montserrat:wght@600;700&amp;display=swap' type='text/css' media='all' />
+        <link rel='stylesheet' id='turner-css'  href='dops/themes/niig/stylecf00.css?368' type='text/css' media='all' />
+        <link rel='stylesheet' id='slick-slider-css'  href='./css/header/slick.css' type='text/css' media='all' />
+        <link rel='stylesheet' id='bvi-css'  href='./css/header/bvi.css' type='text/css' media='all' />
+        <link rel='stylesheet' id='history-css'  href='./css/header/history.css' type='text/css' media='all' />
+        <link rel="icon" href="dops/uploads/2021/03/cropped-cropped-fav-270x270-1-192x192.jpg" sizes="192x192" />
+        <link rel="apple-touch-icon" href="dops/uploads/2021/03/cropped-cropped-fav-270x270-1-180x180.jpg" />
+        <meta name="msapplication-TileImage" content="https://niig/dops/uploads/2021/03/cropped-cropped-fav-270x270-1-270x270.jpg" />
+
+        <body class="home blog">
+        <header class="site-header">
+            <div class="header-top">
+                <ul class="menu top-menu"><li id="menu-item-5135" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-5135"><a href="#" aria-current="page">Главная</a></li>
+                    <li id="menu-item-2815" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2815"><a href="#">Об Институте</a>
+                        <ul class="sub-menu">
+                            <li id="menu-item-11746" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11746"><a href="#">Об Институте</a></li>
+                            <li id="menu-item-10834" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-10834"><a href="#">Сертификаты, лицензия, аккредитация </a></li>
+                            <li id="menu-item-3175" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3175"><a href="#">Руководство</a></li>
+                            <li id="menu-item-1930" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1930"><a href="#">Дирекция</a></li>
+                            <li id="menu-item-6671" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-6671"><a href="#">Руководители отделов</a></li>
+                            <li id="menu-item-4631" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4631"><a href="#">Историческая справка </a></li>
+                            <li id="menu-item-10584" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-10584"><a href="#">Новости</a></li>
+                            <li id="menu-item-2743" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2743"><a href="#">Документы</a></li>
+                            <li id="menu-item-8336" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8336"><a href="#">Противодействие коррупции</a></li>
+                            <li id="menu-item-2941" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2941"><a href="#">Контактная информация</a></li>
+                            <li id="menu-item-201" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-201"><a href=#">Международное сотрудничество</a></li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-2513" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-2513"><a href="#">Наука</a>
+                        <ul class="sub-menu">
+                            <li id="menu-item-6840" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-6840"><a href="#">Учёный совет</a></li>
+                            <li id="menu-item-2734" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2734"><a href="#">Объединенный совет молодых ученых и специалистов</a></li>
+                            <li id="menu-item-2733" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2733"><a href="#">Организационно-методический отдел</a></li>
+                            <li id="menu-item-2732" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2732"><a href="#">Результаты интеллектуальной деятельности</a></li>
+                            <li id="menu-item-2731" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2731"><a href="#">Этический комитет</a></li>
+                            <li id="menu-item-2730" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2730"><a href="#">Они защитились в нашем Институте</a></li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-2771" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2771"><a href="#">Образование</a>
+                        <ul class="sub-menu">
+                            <li id="menu-item-2780" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2780"><a href="#">Основные сведения</a></li>
+                            <li id="menu-item-2782" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2782"><a href="#">Структура и органы управления образовательной организацией</a></li>
+                            <li id="menu-item-7862" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-7862"><a href="#">Документы</a></li>
+                            <li id="menu-item-5132" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5132"><a href="#">Образование</a></li>
+                            <li id="menu-item-2778" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2778"><a href="#">Образовательные стандарты</a></li>
+                            <!--пример следующего уровня меню-->
+                            <li id="menu-item-2779" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2779"><a href="#">Пример следующего уровня меню</a>
+                                <ul class="sub-menu">
+                                    <li id="menu-item-10706" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-10706"><a href="#">1</a></li>
+                                    <li id="menu-item-10707" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-10707"><a href="#">2</a></li>
+                                    <li id="menu-item-10708" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-10708"><a href="#">3</a></li>
+                                </ul>
+                            </li>
+                            <li id="menu-item-2772" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2772"><a href="#">4</a></li>
+                            <li id="menu-item-2776" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2776"><a href="#">5</a></li>
+                            <li id="menu-item-2783" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2783"><a href="#">6</a></li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-2515" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2515"><a href="#">Оценка риска</a>
+                        <ul class="sub-menu">
+                            <li id="menu-item-8176" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-8176"><a href="#">Об оценке риска</a></li>
+                            <li id="menu-item-2567" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2567"><a href="#">Оценка риска для здоровья населения при разработке проекта СЗЗ</a></li>
+                            <li id="menu-item-11747" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-11747"><a href="#">Материалы, предоставляемые заказчиком</a></li>
+                        </ul>
+                    </li>
+                    <li id="menu-item-2516" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2516"><a href="#">НП "Демография"</a></li>
+                    <li id="menu-item-9107" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9107"><a href="#">Центр профпатологии</a></li>
+                    <li id="menu-item-202" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-202"><a href="#">Контакты</a></li>
+                </ul>
+                <div class="lang">
+                    <a class="rus active" href="#">RUS</a>
+                    <a class="en" href="#">EN</a>
+                </div>
+
+                <a class="eye-link button button-yellow" id="eye-link">
+                    Версия для слабовидящих
+                </a>
+            </div>
+
+            <div class="header-center">
+                <div class="logo">
+                    <img width="138" height="144" src="niig/images/logo.png" alt="" />
+
+                    <h1>
+                        <small class="blue">Федеральное бюджетное учреждение науки</small>
+                        <span>Новосибирский научно-исследовательский институт гигиены<br /></span>
+                        <small class="green">Федеральной службы по надзору в сфере защиты прав потребителей и благополучия человека</small>
+                    </h1>
+                </div>
+
+                <div class="tel">
+                    <a href="tel:83833433401"><span>+7(383)</span>343-34-01</a>
+                    <small>Приемная<br>
+                        пн. - пт. 9:00 - 17:40<br>сб., вс. выходной</small>
+                    <a href="tel:83833433401"><span>+7(383)</span>343-34-01</a>
+                    <small>Регистратура Центра Профпатологии<br>
+                        пн. - пт. 9:00 - 15:00<br>сб., вс. выходной</small>
+                </div>
+
+                <div class="contacts contacts-info">
+                    <div class="adress">630000, Новосибирская область, г. Новосибирск, ул. Пархоменко, д. 7</div>
+                    <a href="obratnaya-svyaz/index.html" class="button button-big">Обратная связь</a>
+                </div>
+
+            </div>
+        </header>
+        </body>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Default"
+    }
+</script>
+
+<style scoped>
+    /*
+    Visually impaired button
+*/
+
+    .visually-impaired {
+        font-size: 20px
+    }
+
+    .visually-impaired .header-top {
+        font-size: 18px;
+    }
+
+    .eye-link.button-red {
+        padding: 10px 25px;
+        color: #fff;
+        background-color: #ff7700;
+    }
+
+    .visually-impaired .news-slider .right {
+        max-width: 500px;
+    }
+
+    .visually-impaired .item-desc, .visually-impaired .employees-list .item .item-desc {
+        font-size: 1em;
+        line-height: 1.875;
+    }
+
+    .visually-impaired .news-slider .slider-items {
+        background-color: #fff;
+    }
+
+    .visually-impaired .patient-info-item .h2 {
+        word-wrap: break-word;
+    }
+
+    .visually-impaired .about .top {
+        max-width: 910px;
+        background-color: #fff;
+    }
+
+    .visually-impaired .about .about-item {
+        background-color: #fff;
+    }
+
+    .visually-impaired .footer-contacts>div {
+        flex-basis: 320px;
+    }
+
+    .visually-impaired .footer-contacts .tel a:before {
+        margin-right: 15px;
+    }
+</style>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,44 +2,54 @@ import VueRouter from "vue-router";
 import Vue from 'vue';
 Vue.use(VueRouter);
 
-import ExampleComponent from "./components/ExampleComponent";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Cabinet from "./components/auth/Cabinet";
 import MainComponent from "./components/MainComponent";
+import New from "./components/New";
 
 
 const router = new VueRouter({
 
     mode:'history',
     routes:[
-        {
-            path:'/example',
-            component: ExampleComponent,
-        },
+
         {
             path:"/register",
             component: Register,
             name: 'register'
-
         },
         {
             path:"/login",
             component: Login,
-            name: 'login'
-
+            name: 'login',
+            meta: {
+                layout: 'Empty'
+            }
         },
         {
             path:"/cabinet",
             component: Cabinet,
-            name: 'cabinet'
-
+            name: 'cabinet',
+            meta: {
+                layout: 'Default'
+            }
         },
         {
             path:"",
             component: MainComponent,
-            name: 'main'
-
+            name: 'main',
+            meta: {
+                layout: 'Default'
+            }
+        },
+        {
+            path:"/new",
+            component: New,
+            name: 'new',
+            meta: {
+                layout: 'Default'
+            }
         },
 
     ]
